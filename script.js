17d91871e01250b18ea3b06d3afbcd65cd4e6a03
rafiiddh <muhamadrafid706@gmail.com>
2025-09-27 15:11:01 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.project-card');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.2 });
+
+cards.forEach(card => observer.observe(card));
